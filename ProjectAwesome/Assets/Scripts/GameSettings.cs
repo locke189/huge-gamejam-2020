5fde3c3bf109f5ed04ade4eacfb920366c659126
Jose Gutierrez <jogutierrez@hugeinc.com>
2020-02-01 17:06:07 -0500

    Move Scripts to Script folder

--- ProjectAwesome/Assets/Scripts/GameSettings.cs
+++ ProjectAwesome/Assets/Scripts/GameSettings.cs
@@ -8,5 +8,5 @@ public class GameSettings : ScriptableObject
     public int difficulty = 1;
     public string playerOneName = "Player 1";
     public string playerTwoName = "Player 2";
-    public static string playerWinner = "WinnerName";
+    public string playerWinner = "WinnerName";
 }
--- ProjectAwesome/Assets/Scripts/showWinnerName.cs
+++ ProjectAwesome/Assets/Scripts/showWinnerName.cs
@@ -3,10 +3,11 @@ using UnityEngine.UI;
 
 public class showWinnerName : MonoBehaviour
 {
+    [SerializeField] public GameSettings settings;
     public Text WinnerName;
 
     private void Start()
     {
-        WinnerName.text = GameSettings.playerWinner;
+        WinnerName.text = settings.playerWinner;
     }
 }
