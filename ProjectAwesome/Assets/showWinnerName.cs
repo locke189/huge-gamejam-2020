33bab83e8ab9c6069a0cc92e684db80ef6c7ee88
Jose Gutierrez <jogutierrez@hugeinc.com>
2020-02-01 17:03:26 -0500

    Add Results Screen elements

---
+++ ProjectAwesome/Assets/showWinnerName.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class showWinnerName : MonoBehaviour
+{
+    public Text WinnerName;
+
+    private void Start()
+    {
+        WinnerName.text = GameSettings.playerWinner;
+    }
+}
