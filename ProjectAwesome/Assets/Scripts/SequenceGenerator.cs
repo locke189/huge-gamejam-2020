cfa26bb9f13cb5b74525be001454f45d86fd2b65
Juan David Ramirez <juramirez@hugeinc.com>
2020-02-01 18:38:34 -0500

    arrow and tool sequence generation linked to game state

--- ProjectAwesome/Assets/Scripts/SequenceGenerator.cs
+++ ProjectAwesome/Assets/Scripts/SequenceGenerator.cs
@@ -7,38 +7,6 @@ public class SequenceGenerator : MonoBehaviour
 {
     public System.Random rand = new System.Random();
 
-    //public Pair[] GenerateSequence(int difficulty) {
-
-    //    Pair[] Sequence = new Pair[difficulty*10];
-
-    //    int[] AvailableTools = { 1, 2, 3, 4 };
-
-    //    int[] SelectedTools = new int[difficulty + 1];
-
-
-
-    //    for (int i = 0; i <= difficulty; i++)
-    //    {
-    //        int randomTool = rand.Next(0, 3);
-    //        SelectedTools[i] = AvailableTools[randomTool];
-    //        AvailableTools = Array.FindAll(AvailableTools, val => val != randomTool);
-
-    //    }
-
-    //    string[] Arrows = { "UP", "DOWN", "LEFT", "RIGHT" };
-
-    //    for (int i = 0; i < difficulty*10; i++)
-    //    {
-    //        Debug.Log("entry added");
-    //        int pairToolPos = rand.Next(0, SelectedTools.Length);
-    //        int pairArrowPos = rand.Next(0, 3);
-    //        Pair entry = new Pair(SelectedTools[pairToolPos], Arrows[pairArrowPos]);
-    //        Sequence[i] = entry;
-    //    }
-    //    return Sequence;
-
-    //}
-
     public int[] GenerateToolSequence(int difficulty) {
         int[] Sequence = new int[difficulty * 10];
 
