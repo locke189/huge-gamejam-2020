d05e2a1169e1b4a00bae7b3f59dc0409ae88ca83
Juan David Ramirez <juramirez@hugeinc.com>
2020-02-01 18:07:56 -0500

    rebase master

--- ProjectAwesome/Assets/Scripts/SequenceGenerator.cs
+++ ProjectAwesome/Assets/Scripts/SequenceGenerator.cs
@@ -5,16 +5,46 @@ using UnityEngine;
 
 public class SequenceGenerator : MonoBehaviour
 {
+    public System.Random rand = new System.Random();
 
-    public Pair[] GenerateSequence(int difficulty) {
+    //public Pair[] GenerateSequence(int difficulty) {
 
-        Pair[] Sequence = new Pair[difficulty*10];
+    //    Pair[] Sequence = new Pair[difficulty*10];
+
+    //    int[] AvailableTools = { 1, 2, 3, 4 };
+
+    //    int[] SelectedTools = new int[difficulty + 1];
+
+
+
+    //    for (int i = 0; i <= difficulty; i++)
+    //    {
+    //        int randomTool = rand.Next(0, 3);
+    //        SelectedTools[i] = AvailableTools[randomTool];
+    //        AvailableTools = Array.FindAll(AvailableTools, val => val != randomTool);
+
+    //    }
+
+    //    string[] Arrows = { "UP", "DOWN", "LEFT", "RIGHT" };
+
+    //    for (int i = 0; i < difficulty*10; i++)
+    //    {
+    //        Debug.Log("entry added");
+    //        int pairToolPos = rand.Next(0, SelectedTools.Length);
+    //        int pairArrowPos = rand.Next(0, 3);
+    //        Pair entry = new Pair(SelectedTools[pairToolPos], Arrows[pairArrowPos]);
+    //        Sequence[i] = entry;
+    //    }
+    //    return Sequence;
+
+    //}
+
+    public int[] GenerateToolSequence(int difficulty) {
+        int[] Sequence = new int[difficulty * 10];
 
         int[] AvailableTools = { 1, 2, 3, 4 };
 
         int[] SelectedTools = new int[difficulty + 1];
-
-        System.Random rand = new System.Random();
 
         for (int i = 0; i <= difficulty; i++)
         {
@@ -23,19 +53,25 @@ public class SequenceGenerator : MonoBehaviour
             AvailableTools = Array.FindAll(AvailableTools, val => val != randomTool);
 
         }
-
-        string[] Arrows = { "UP", "DOWN", "LEFT", "RIGHT" };
-
-        for (int i = 0; i < difficulty*10; i++)
+        for (int i = 0; i < difficulty * 10; i++)
         {
-            Debug.Log("entry added");
-            int pairToolPos = rand.Next(0, SelectedTools.Length);
-            int pairArrowPos = rand.Next(0, 3);
-            Pair entry = new Pair(SelectedTools[pairToolPos], Arrows[pairArrowPos]);
+            int toolPos = rand.Next(0, SelectedTools.Length);
+            int entry = SelectedTools[toolPos];
             Sequence[i] = entry;
         }
         return Sequence;
+    }
 
+    public String[] GenerateArrowSequence(int difficulty) {
+        String[] Sequence = new String[difficulty * 10];
+        string[] Arrows = { "UP", "DOWN", "LEFT", "RIGHT" };
+        for (int i = 0; i < difficulty * 10; i++)
+        {
+            int arrowPos = rand.Next(0, 3);
+            String entry = Arrows[arrowPos];
+            Sequence[i] = entry;
+        }
+        return Sequence;
     }
 
 }
