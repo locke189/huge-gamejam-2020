c80315595ed3549b00c33f5a80ca3947de07545e
Maria Camila Gomez <camigomez@hugeinc.com>
2020-02-01 15:36:56 -0500

    Create elements from Menu and get names

---
+++ ProjectAwesome/Assets/Scripts/MenuScreenController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScreenController : MonoBehaviour
+{
+    public InputField playerOneName;
+    public InputField playerTwoName;
+
+    public void startGame()
+    {
+        Debug.Log("Player 1 name: " + playerOneName.text);
+        Debug.Log("Player 2 name: " + playerTwoName.text);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
