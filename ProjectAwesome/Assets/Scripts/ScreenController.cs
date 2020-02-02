eed956c4d6cc5febc1a9c38110d69c78a7c8f1b6
Jose Gutierrez <jogutierrez@hugeinc.com>
2020-02-02 15:13:16 -0500

    Fix screen controller

--- ProjectAwesome/Assets/Scripts/ScreenController.cs
+++ ProjectAwesome/Assets/Scripts/ScreenController.cs
@@ -30,15 +30,6 @@ public class ScreenController : MonoBehaviour
 	public void LoadNextScreen()
 	{
 		int index = SceneManager.GetActiveScene().buildIndex;
-		Debug.Log("next");
-		if (index + 1 > 1)
-		{
-			Debug.Log("next");
-			GameObject splashAudio = GameObject.Find("MusicControllerSplash");
-			splashAudio.GetComponent<AudioSource>().Stop();
-			Destroy(splashAudio);
-		}
-
 		SceneManager.LoadScene(index + 1);
 	}
 
