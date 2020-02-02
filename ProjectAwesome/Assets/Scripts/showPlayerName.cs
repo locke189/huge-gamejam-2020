fe9a7f8e750afc46cb6169c7ae3de803aaf907c4
Jose Gutierrez <jogutierrez@hugeinc.com>
2020-02-01 20:29:42 -0500

    Add Player card prefab

---
+++ ProjectAwesome/Assets/Scripts/showPlayerName.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class showPlayerName : MonoBehaviour
+{
+    [SerializeField] public GameSettings settings;
+    public Text PlayerName;
+    public int PlayerNumber;
+
+    private void Start()
+    {
+        if(PlayerNumber == 1)
+        {
+            PlayerName.text = settings.playerOneName;
+        }
+        else
+        {
+            PlayerName.text = settings.playerTwoName;
+        }
+    }
+}
