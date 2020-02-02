8dc95661ec238e5fe0823c2f2524086459640fa9
Juan Insuasti <jinsuasti@hugeinc.com>
2020-02-01 22:26:05 -0500

    arrow component

--- ProjectAwesome/Assets/Scripts/GameController.cs
+++ ProjectAwesome/Assets/Scripts/GameController.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] GameState state;
+    [SerializeField] GameSettings settings;
+    [SerializeField] UnityEvent onSetupComplete;
     SequenceGenerator ArrowSequence = new SequenceGenerator();
     SequenceGenerator ToolSequence = new SequenceGenerator();
+    
     // Start is called before the first frame update
     void Start()
     {
-        loadSet(2);
+        loadSet(settings.difficulty);
+        //starts the gaming sequence
+        onSetupComplete.Invoke();
+        state.gameStateName = "PLAY";
     }
 
     // Update is called once per frame
@@ -19,11 +27,15 @@ public class GameController : MonoBehaviour
 
     }
 
-    void loadSet(int dif) {
+    public void loadSet(int dif) {
         string[] ArrowSet = ArrowSequence.GenerateArrowSequence(dif);
         int[] ToolSet = ToolSequence.GenerateToolSequence(dif);
         Debug.Log("load set");
+        Array.Resize(ref state.arrows, ArrowSet.Length);
+        Array.Resize(ref state.tools, ToolSet.Length);
         state.arrows = ArrowSet;
         state.tools = ToolSet;
     }
+
+
 }
